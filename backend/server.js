53ee7b1ce947006ab3fd6313591eb35a1856b1a7
Harry Newton <hazzaprime@gmail.com>
2019-08-31 19:13:18 +1000

    started on frontend and backend, frontend needs fixing

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const extractAudio = require('ffmpeg-extract-audio');
+
 
 const app = express();
 
-app.get('/',(req,res) => {
-    res.json({message:'WELCOME'});
+app.get('/', (req, res) => {
+    res.send('This is the index page');
 })
 
-app.listen(8080, () => console.log('Server started on port 3000'));
+app.post('/upload', function (req, res) {
+    var today = new Date();
+    var dateStr = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    extractAudio({
+        input: req.files.video,
+        output: 'extracted_audios/video_audio_' + dateStr + '.mp3'
+    });
+
+    // getInfo
+
+});
+
+app.listen(8080, () => console.log('Server started on port 8080'));
